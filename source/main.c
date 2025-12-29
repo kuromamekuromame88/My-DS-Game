ea926798862782d467092d0389e0b103a5dfdc93
kuromamekuromame88 <tei8sia@outlook.jp>
2025-12-29 22:19:39 +0900

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,1 +1,17 @@
+#include <nds.h>
+#include <stdio.h>
+
+int main(void) {
+    // 上画面をテキストコンソールに
+    consoleDemoInit();
+
+    iprintf("Hello Nintendo DS!\n");
+    iprintf("Built with GitHub Actions\n");
+
+    while (1) {
+        swiWaitForVBlank();
+    }
+
+    return 0;
+}
 
